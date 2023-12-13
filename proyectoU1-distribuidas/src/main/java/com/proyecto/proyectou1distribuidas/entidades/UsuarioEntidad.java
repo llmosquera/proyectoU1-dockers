d8b23d1eb25a434af0be5121185ce9cc17eb7a79
llmosquera <lmmosquera@espe.edu.ec>
2023-12-13 16:57:41 -0500

    Update UsuarioEntidad.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/UsuarioEntidad.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/UsuarioEntidad.java
@@ -1,4 +1,12 @@
 package com.proyecto.proyectou1distribuidas.entidades;
 
+import lombok.Data;
+
+@Data
 public class UsuarioEntidad {
+    private Long id_usuario;
+    private String nombre_usuario;
+    private String contrasenia_usuario;
+    private String rol_usuario;
+
 }
