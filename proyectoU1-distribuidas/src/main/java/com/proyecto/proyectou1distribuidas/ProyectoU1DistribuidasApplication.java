e279538322d62d61bbc6a3cafc5f6c9d5683e2ec
llmosquera <lmmosquera@espe.edu.ec>
2023-12-13 16:40:57 -0500

    Creacion Proyecto Sprign

---
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/ProyectoU1DistribuidasApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectou1distribuidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU1DistribuidasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoU1DistribuidasApplication.class, args);
+    }
+
+}
---
+++ proyectoU1-distribuidas/src/test/java/com/proyecto/proyectou1distribuidas/ProyectoU1DistribuidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectou1distribuidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoU1DistribuidasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
