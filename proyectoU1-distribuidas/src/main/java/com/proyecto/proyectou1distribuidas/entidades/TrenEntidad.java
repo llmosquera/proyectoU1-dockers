c12f7094171b364f84fb44115063d023983a6ebb
llmosquera <lmmosquera@espe.edu.ec>
2023-12-13 16:53:17 -0500

    Update TrenEntidad.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/TrenEntidad.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/TrenEntidad.java
@@ -1,4 +1,16 @@
 package com.proyecto.proyectou1distribuidas.entidades;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class TrenEntidad {
+
+    private Long id_tren;
+    private String modelo_tren;
+    private String capacidad;
+    private Boolean estado;
+
+    private List<EstacionEntidad> id_estacion;
 }
