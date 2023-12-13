c21954c0ef432614ceb2c36409c5f3a39e6f9a83
llmosquera <lmmosquera@espe.edu.ec>
2023-12-13 16:55:49 -0500

    Update HoraioEntidad.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/HoraioEntidad.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/HoraioEntidad.java
@@ -1,4 +1,22 @@
 package com.proyecto.proyectou1distribuidas.entidades;
 
+import lombok.Data;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class HoraioEntidad {
+
+    private Long id_horario;
+    private LocalDate dias_semana;
+    private Time hora_salida;
+    private Time hora_llegada;
+
+
+    //relacion de muchos a uno con ruta
+    private List<RutaEntidad> id_ruta;
+    //relacion de muchos a uno con tren
+    private List<TrenEntidad> id_tren;
 }
