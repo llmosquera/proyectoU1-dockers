1624f829d1e7b01f8d89ba70e16f9cfb56560890
llmosquera <lmmosquera@espe.edu.ec>
2023-12-13 16:52:08 -0500

    Update EstacionEntidad.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/EstacionEntidad.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/EstacionEntidad.java
@@ -1,4 +1,13 @@
 package com.proyecto.proyectou1distribuidas.entidades;
 
+import lombok.Data;
+
+@Data
 public class EstacionEntidad {
+    private Long id_estacion;
+    private String nombre_estacion;
+    private String ubicacion_estacion;
+    private String plataformas_estacion;
+    private String estacion_origen;
+    private String estacion_destino;
 }
