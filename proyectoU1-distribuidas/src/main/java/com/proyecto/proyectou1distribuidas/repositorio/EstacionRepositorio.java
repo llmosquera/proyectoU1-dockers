3ddb6a4fb11a02539ca838da483c29fbcd751caf
Raquel <orgarces@espe.edu.ec>
2023-12-13 17:09:23 -0500

    Update EstacionRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/EstacionRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/EstacionRepositorio.java
@@ -14,10 +14,29 @@ public class EstacionRepositorio {
 
     private Map<Long, EstacionEntidad> estacion;
     private AtomicLong id_generador;
-    
+
     public EstacionRepositorio() {
         this.estacion = new HashMap<>();
         this.id_generador = new AtomicLong(0);
+    }
+
+    //metodo para guardar una estacion
+    public EstacionEntidad guardarEstacion(EstacionEntidad estacionEntidad){
+        if(estacionEntidad.getId_estacion() !=null){
+            //la estacion ya tiene un id, por lo tanto es uina actualizacion
+            if(estacion.containsKey(estacionEntidad.getId_estacion())){
+                //reemplazar la estacion
+                estacion.put(estacionEntidad.getId_estacion(), estacionEntidad);
+                return estacionEntidad;
+            }else {
+                throw new IllegalArgumentException("Estacion no encontrado para el ID: " + estacionEntidad.getId_estacion());
+            }
+        }else {
+            long id_estacion = id_generador.incrementAndGet();
+            estacionEntidad.setId_estacion(id_estacion);
+            estacion.put(id_estacion, estacionEntidad);
+            return estacionEntidad;
+        }
     }
 
 }
