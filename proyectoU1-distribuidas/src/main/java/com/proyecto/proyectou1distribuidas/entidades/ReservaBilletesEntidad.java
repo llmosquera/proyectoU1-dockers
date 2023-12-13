8b254138b08889977a0576ef1c274f8bab3d93ac
llmosquera <lmmosquera@espe.edu.ec>
2023-12-13 16:56:53 -0500

    Update ReservaBilletesEntidad.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/ReservaBilletesEntidad.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/entidades/ReservaBilletesEntidad.java
@@ -2,6 +2,19 @@ package com.proyecto.proyectou1distribuidas.entidades;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ReservaBilletesEntidad {
+    private Long id_reserva;
+
+    private String detalle_pasajero;
+    private Integer num_asientos_reservados;
+    private Boolean estado_reserva;
+
+    //relacion de miuchos a unos con usuario
+    private List<UsuarioEntidad> id_usuario;
+
+    //relaion de muchos a unos con horario
+    private List<HorarioEntidad> id_horario;
 }
