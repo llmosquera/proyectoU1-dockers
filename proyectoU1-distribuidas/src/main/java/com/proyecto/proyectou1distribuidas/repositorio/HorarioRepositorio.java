44e2543c76c11eddc3bf04f5b49f482fa875ec1c
Raquel <orgarces@espe.edu.ec>
2023-12-14 17:37:42 -0500

    Update HorarioRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/HorarioRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/HorarioRepositorio.java
@@ -1,9 +1,12 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
 import com.proyecto.proyectou1distribuidas.entidades.HorarioEntidad;
+import com.proyecto.proyectou1distribuidas.entidades.RutaEntidad;
+import com.proyecto.proyectou1distribuidas.entidades.TrenEntidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -21,5 +24,33 @@ public class HorarioRepositorio {
     public HorarioRepositorio() {
         this.horarios = new HashMap<>();
         this.id_generador = new AtomicLong(0);
+    }
+
+    public HorarioEntidad guardarHorario(HorarioEntidad horarioEntidad,
+                                         Long id_ruta, Long id_tren){
+
+        //obtener la ruta y el tren pos sus IDs
+        RutaEntidad ruta = rutaRepositorio.getById(id_ruta).orElseThrow(() ->
+                new RuntimeException("Ruta con ID " + id_ruta + " no encontrada."));
+        TrenEntidad tren = trenRepositorio.getById(id_tren)
+                .orElseThrow(() -> new IllegalArgumentException("Tren con ID " + id_tren + " no encontrado."));
+
+        // Inicializar las listas si son nulas
+        if (horarioEntidad.getId_ruta() == null) {
+            horarioEntidad.setId_ruta(new ArrayList<>());
+        }
+        if (horarioEntidad.getId_tren() == null) {
+            horarioEntidad.setId_tren(new ArrayList<>());
+        }
+
+        //asiganar la ruta y el tren al horario
+        horarioEntidad.getId_ruta().add(ruta);
+        horarioEntidad.getId_tren().add(tren);
+        // Generar un nuevo ID para el horario
+        Long idHorario = id_generador.incrementAndGet();
+        horarioEntidad.setId_horario(idHorario);
+        // Guardar el horario en el repositorio
+        horarios.put(idHorario, horarioEntidad);
+        return horarioEntidad;
     }
 }
