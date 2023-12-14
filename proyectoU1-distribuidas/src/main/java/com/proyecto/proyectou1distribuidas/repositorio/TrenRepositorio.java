d018d0c7eb3ac2b716d53d4719653e04b0b65e15
llmosquera <lmmosquera@espe.edu.ec>
2023-12-14 17:50:18 -0500

    Update TrenRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/TrenRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/TrenRepositorio.java
@@ -1,7 +1,11 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import com.proyecto.proyectou1distribuidas.entidades.TrenEntidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 
@@ -9,4 +13,7 @@ public class TrenRepositorio {
 
     @Autowired
     public EstacionRepositorio repositorio;
+
+    private Map<Long, TrenEntidad> trenes;
+    private AtomicLong id_generador;
 }
