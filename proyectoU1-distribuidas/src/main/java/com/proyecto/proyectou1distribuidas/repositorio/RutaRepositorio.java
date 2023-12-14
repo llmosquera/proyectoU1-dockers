8a7d11edd0b739470cf0e2ee954d484138321d80
llmosquera <lmmosquera@espe.edu.ec>
2023-12-14 17:46:36 -0500

    Update RutaRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/RutaRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/RutaRepositorio.java
@@ -1,12 +1,20 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import com.proyecto.proyectou1distribuidas.entidades.RutaEntidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class RutaRepositorio {
 
     @Autowired
     public EstacionRepositorio repositorio;
+
+    private Map<Long, RutaEntidad> rutas;
+    private AtomicLong id_generador;
+
 
 }
