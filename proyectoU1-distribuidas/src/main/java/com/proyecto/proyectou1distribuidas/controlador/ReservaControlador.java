f51f214a5f6bd433609c9e64e167b4530e5eeb95
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:24:25 -0500

    Update ReservaControlador.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/controlador/ReservaControlador.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/controlador/ReservaControlador.java
@@ -1,4 +1,9 @@
 package com.proyecto.proyectou1distribuidas.controlador;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservas")
 public class ReservaControlador {
 }
