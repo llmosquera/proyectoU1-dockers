cd4becde99328f4632151bbbad88d5a43d841b58
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:08:26 -0500

    Update EstacionService.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/EstacionService.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/EstacionService.java
@@ -1,5 +1,6 @@
 package com.proyecto.proyectou1distribuidas.servicios;
 
+import com.proyecto.proyectou1distribuidas.entidades.EstacionEntidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
