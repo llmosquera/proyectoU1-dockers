9b2c8fc40104b868a7c3f61fab93b5387c3865f7
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:23:53 -0500

    Update HorarioControlador.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/controlador/HorarioControlador.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/controlador/HorarioControlador.java
@@ -1,9 +1,44 @@
 package com.proyecto.proyectou1distribuidas.controlador;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.proyecto.proyectou1distribuidas.entidades.HorarioEntidad;
+import com.proyecto.proyectou1distribuidas.servicios.HorarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/horarios")
 public class HorarioControlador {
+
+    @Autowired
+    public HorarioService service;
+
+    @PostMapping(value = "/insertar/{id_ruta}/{id_tren}", consumes = "application/json")
+    public HorarioEntidad guardarHorario(@RequestBody HorarioEntidad horarioEntidad,
+                                         @PathVariable Long id_ruta,
+                                         @PathVariable Long id_tren){
+
+        return service.guardarHorario(horarioEntidad, id_ruta, id_tren);
+    }
+
+    //actualizar
+    @PutMapping("/actualizar/{id_horario}")
+    public HorarioEntidad actualizarHorario(@RequestBody HorarioEntidad horarioEntidad,
+                                            @PathVariable Long id_horario){
+
+        return service.actualizarHorario(horarioEntidad,id_horario);
+    }
+
+    //eliminar
+    @DeleteMapping("/eliminar/{id_horario}")
+    public void eliminarHorario(@PathVariable Long id_horario){
+        service.eliminarHorario(id_horario);
+    }
+
+    //listarTdos
+    @GetMapping("/listar/todos")
+    public List<HorarioEntidad> obtenerTodos(){
+        return service.obtenerTodos();
+    }
 }
