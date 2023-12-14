d1603ad201665403049405534752a218acae2451
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:10:51 -0500

    Update ReservaServices.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/ReservaServices.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/ReservaServices.java
@@ -1,4 +1,12 @@
 package com.proyecto.proyectou1distribuidas.servicios;
 
+import com.proyecto.proyectou1distribuidas.repositorio.ReservaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservaServices {
+
+    @Autowired
+    public ReservaRepositorio repositorio;
 }
