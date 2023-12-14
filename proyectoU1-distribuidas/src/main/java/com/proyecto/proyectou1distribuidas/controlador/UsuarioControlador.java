819300003b924003575a9a8bd7daf46ebd740417
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:27:06 -0500

    Update UsuarioControlador.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/controlador/UsuarioControlador.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/controlador/UsuarioControlador.java
@@ -1,9 +1,47 @@
 package com.proyecto.proyectou1distribuidas.controlador;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.proyecto.proyectou1distribuidas.entidades.UsuarioEntidad;
+import com.proyecto.proyectou1distribuidas.servicios.UsuariosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioControlador {
+    @Autowired
+    public UsuariosService service;
+
+    @PostMapping("/guardar")
+    public UsuarioEntidad guardarUsuario(@RequestBody UsuarioEntidad usuarioEntidad) {
+        return service.guardarUsuario(usuarioEntidad);
+    }
+
+    //obtener por id
+    @GetMapping("/obtener/{id_usuario}")
+    public UsuarioEntidad getUsuarioById(Long id_usuario){
+
+        return service.getUsuarioById(id_usuario);
+    }
+
+
+    //obtener todos;
+    @GetMapping("/obtener")
+    public List<UsuarioEntidad> obtenerTodasEstaciones(){
+        return service.obtenerTodosUsuarios();
+    }
+
+    //metodo para elmiminar
+    @DeleteMapping("/eliminar/{id_usuario}")
+    public void eliminarEstacion(@PathVariable Long id_usuario){
+        service.eliminarUsuario(id_usuario);
+    }
+
+
+    @PutMapping("/actualizar/{id_usuario}")
+    public UsuarioEntidad actualizarUsuario(@RequestBody UsuarioEntidad usuarioEntidad,
+                                            @PathVariable Long id_usuario){
+        return service.actualizar(usuarioEntidad, id_usuario);
+    }
 }
