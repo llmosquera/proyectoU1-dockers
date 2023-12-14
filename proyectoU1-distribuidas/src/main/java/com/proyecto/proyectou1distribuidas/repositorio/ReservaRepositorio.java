cb725bd8023d0477dea54d989c00ef971c9df51e
Raquel <orgarces@espe.edu.ec>
2023-12-14 17:42:12 -0500

    updates

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/ReservaRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/ReservaRepositorio.java
@@ -1,4 +1,7 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ReservaRepositorio {
 }
--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/RutaRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/RutaRepositorio.java
@@ -1,4 +1,8 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
+
 public class RutaRepositorio {
 }
