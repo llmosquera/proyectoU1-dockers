26b6692851c38c4add194eae87279bade3325e16
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:09:43 -0500

    Update HorarioService.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/HorarioService.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/HorarioService.java
@@ -1,7 +1,12 @@
 package com.proyecto.proyectou1distribuidas.servicios;
 
+import com.proyecto.proyectou1distribuidas.repositorio.HorarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HorarioService {
+
+    @Autowired
+    public HorarioRepositorio repositorio;
 }
