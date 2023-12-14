49f086ddd37f6350c60758b5514380ffaa24a11c
llmosquera <lmmosquera@espe.edu.ec>
2023-12-14 17:50:04 -0500

    Update TrenRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/TrenRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/TrenRepositorio.java
@@ -1,8 +1,12 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public class TrenRepositorio {
+
+    @Autowired
+    public EstacionRepositorio repositorio;
 }
