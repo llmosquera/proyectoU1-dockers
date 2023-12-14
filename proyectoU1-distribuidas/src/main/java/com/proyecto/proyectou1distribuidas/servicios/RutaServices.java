bcb8a21bf2e77fde072160a165c1c24032ae61c1
RaquelG18 <orgarces@espe.edu.ec>
2023-12-14 18:13:08 -0500

    Update RutaServices.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/RutaServices.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/servicios/RutaServices.java
@@ -1,4 +1,38 @@
 package com.proyecto.proyectou1distribuidas.servicios;
 
+import com.proyecto.proyectou1distribuidas.entidades.RutaEntidad;
+import com.proyecto.proyectou1distribuidas.repositorio.RutaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class RutaServices {
+    @Autowired
+    public RutaRepositorio repositorio;
+
+    public RutaServices() {
+        this.repositorio = new RutaRepositorio();
+    }
+    public RutaEntidad guardarRutaConSuTren(RutaEntidad rutaEntidad, Long id_estacion){
+        return repositorio.guardarRutaConSuTren(rutaEntidad, id_estacion);
+    }
+
+    public RutaEntidad actualizarRuta(RutaEntidad rutaEntidad, Long id_ruta){
+        return repositorio.actualizarRuta(rutaEntidad, id_ruta);
+    }
+    public void eliminarRuta(Long id_ruta){
+        repositorio.eliminarRuta(id_ruta);
+    }
+
+    public Optional<RutaEntidad> getById(Long id_ruta){
+        return repositorio.getById(id_ruta);
+    }
+    //listarTdos
+    public List<RutaEntidad> obtenerTodos(){
+        return repositorio.obtenerTodos();
+    }
+
 }
