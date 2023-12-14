5ba2bc14d3eeda4368d98eaaa147d6b2a568c19d
Raquel <orgarces@espe.edu.ec>
2023-12-14 17:36:52 -0500

    Update HorarioRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/HorarioRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/HorarioRepositorio.java
@@ -1,8 +1,19 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import com.proyecto.proyectou1distribuidas.entidades.HorarioEntidad;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Map;
 
+@Repository
 public class HorarioRepositorio {
+
+    @Autowired
+    public RutaRepositorio rutaRepositorio;
+    @Autowired
+    public TrenRepositorio trenRepositorio;
+
+    private Map<Long, HorarioEntidad> horarios;
+
 }
