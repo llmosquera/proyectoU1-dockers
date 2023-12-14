d0ca3c61c3106681040a7e91c7b34eaca324135c
llmosquera <lmmosquera@espe.edu.ec>
2023-12-14 17:53:48 -0500

    Update UsuarioRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/UsuarioRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/UsuarioRepositorio.java
@@ -1,8 +1,19 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import com.proyecto.proyectou1distribuidas.entidades.UsuarioEntidad;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class UsuarioRepositorio {
+    private Map<Long, UsuarioEntidad> usuarios;
+    private AtomicLong id_generador;
+
+    public UsuarioRepositorio() {
+        this.usuarios = new HashMap<>();
+        this.id_generador = new AtomicLong(0);
+    }
 }
