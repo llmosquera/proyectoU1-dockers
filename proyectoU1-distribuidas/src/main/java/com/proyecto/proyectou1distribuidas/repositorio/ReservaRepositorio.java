1e8bee3f2f3d2d95b009031ad9a5dbdc363dcd5f
llmosquera <lmmosquera@espe.edu.ec>
2023-12-14 17:55:45 -0500

    Update ReservaRepositorio.java

--- proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/ReservaRepositorio.java
+++ proyectoU1-distribuidas/src/main/java/com/proyecto/proyectou1distribuidas/repositorio/ReservaRepositorio.java
@@ -1,7 +1,13 @@
 package com.proyecto.proyectou1distribuidas.repositorio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReservaRepositorio {
+
+    @Autowired
+    public HorarioRepositorio horarioRepositorio;
+    @Autowired
+    public UsuarioRepositorio usuarioRepositorio;
 }
