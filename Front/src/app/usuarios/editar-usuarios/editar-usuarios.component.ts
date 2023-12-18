1c5a288838cbd360dc294fe134b8810193402ac8
MDCSH <mdsantin@espe.edu.ec>
2023-12-18 18:29:08 -0500

    Guardar y editar usuario

--- Front/src/app/usuarios/editar-usuarios/editar-usuarios.component.ts
+++ Front/src/app/usuarios/editar-usuarios/editar-usuarios.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Usuarios } from '../usuarios';
+import { UsuariosService } from '../usuarios.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-editar-usuarios',
@@ -6,5 +10,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-usuarios.component.css']
 })
 export class EditarUsuariosComponent {
+  usuarios:Usuarios = new Usuarios();
+  
 
+  constructor(private usuarioService:UsuariosService, private enruta:ActivatedRoute, private router:Router){}
+
+  ngOnInit(): void {
+
+    const id_usuario = this.enruta.snapshot.params['id_usuario'];
+
+
+    this.usuarioService.obtenerUsuarioPorId(id_usuario).subscribe(
+      (estacionObtenida) => {
+        this.usuarios = estacionObtenida;
+      },
+      (error) => {
+        console.error('Error al obtener la estación:', error);
+      }
+    );
+  }
+
+  editarUsuario(id_usuario: number) {
+    if(this.validarFormulario()){
+      this.usuarioService.actualizarUsuario(this.usuarios, id_usuario).subscribe(
+        (estacionActualizada) => {
+          console.log('Estación actualizada correctamente:', estacionActualizada);
+          this.irListaUsuarios();
+        },
+        (error) => {
+          console.error('Error al actualizar la estación:', error);
+        });
+      }
+    }
+  irListaUsuarios(){
+    this.router.navigate(['/usuarios'])
+
+  }
+
+  validarFormulario(): boolean {
+    // Validación de campos vacíos
+    if (
+      !this.usuarios.nombre_usuario ||
+      !this.usuarios.contrasenia_usuario ||
+      !this.usuarios.rol_usuario
+    ) {
+      Swal.fire('Error', 'Todos los campos son obligatorios', 'error');
+      return false;
+    }
+
+    // Validación de solo texto en nombre_usuario
+    const soloTextoRegex = /^[a-zA-Z\s]*$/;
+
+    if (!soloTextoRegex.test(this.usuarios.nombre_usuario)) {
+      Swal.fire('Error', 'Solo se permiten letras en el nombre de usuario', 'error');
+      return false;
+    }
+
+    // Validación de contraseña
+    // Validación de contraseña (exactamente 6 caracteres)
+const contraseniaRegex = /^(?=.*[A-Z])(?=.*\W).{6}$/;
+
+if (!contraseniaRegex.test(this.usuarios.contrasenia_usuario)) {
+  Swal.fire('Error', 'La contraseña debe tener exactamente 6 caracteres, comenzar con una letra mayúscula y terminar con un carácter especial (*)', 'error');
+  return false;
+}
+
+
+    return true;
+  }
 }
--- Front/src/app/usuarios/guardar-usuario/guardar-usuario.component.ts
+++ Front/src/app/usuarios/guardar-usuario/guardar-usuario.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuarios } from '../usuarios';
+import { UsuariosService } from '../usuarios.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-guardar-usuario',
   templateUrl: './guardar-usuario.component.html',
   styleUrls: ['./guardar-usuario.component.css']
 })
-export class GuardarUsuarioComponent {
+export class GuardarUsuarioComponent implements OnInit {
+  usuarios: Usuarios = new Usuarios();
+  constructor(private httpUsuarios: UsuariosService, private router: Router) { }
+
+  ngOnInit() { }
+
+
+  //metodo paar guardar
+  //Método para guardar
+  public guardarUsuario() {
+    if (this.validarFormulario()) {
+      this.httpUsuarios.guardarUsuario(this.usuarios).subscribe({
+        next: (datos) => {
+          // Ir a la otra página
+          this.irListaUsuarios();
+        },
+      });
+    }
+  }
+  irListaUsuarios() {
+    this.router.navigate(["/usuarios"])
+  }
+
+  validarFormulario(): boolean {
+    // Validación de campos vacíos
+    if (
+      !this.usuarios.nombre_usuario ||
+      !this.usuarios.contrasenia_usuario ||
+      !this.usuarios.rol_usuario
+    ) {
+      Swal.fire('Error', 'Todos los campos son obligatorios', 'error');
+      return false;
+    }
+
+    // Validación de solo texto en nombre_usuario
+    const soloTextoRegex = /^[a-zA-Z\s]*$/;
+
+    if (!soloTextoRegex.test(this.usuarios.nombre_usuario)) {
+      Swal.fire('Error', 'Solo se permiten letras en el nombre de usuario', 'error');
+      return false;
+    }
+
+    // Validación de contraseña
+    // Validación de contraseña (exactamente 6 caracteres)
+    const contraseniaRegex = /^(?=.*[A-Z])(?=.*\W).{6}$/;
+
+    if (!contraseniaRegex.test(this.usuarios.contrasenia_usuario)) {
+      Swal.fire('Error', 'La contraseña debe tener exactamente 6 caracteres, comenzar con una letra mayúscula y terminar con un carácter especial (*)', 'error');
+      return false;
+    }
+
+
+
+    return true;
+  }
 
 }
