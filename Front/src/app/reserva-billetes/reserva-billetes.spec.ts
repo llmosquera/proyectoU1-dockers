10c20b6e988f14c5929f09b3e482a64ff4464252
Fernando <fjmasache@espe.edu.ec>
2023-12-18 20:58:07 -0500

    reserva billetes spec

---
+++ Front/src/app/reserva-billetes/reserva-billetes.spec.ts
@@ -0,0 +1,7 @@
+import { ReservaBilletes } from './reserva-billetes';
+
+describe('ReservaBilletes', () => {
+  it('should create an instance', () => {
+    expect(new ReservaBilletes()).toBeTruthy();
+  });
+});
